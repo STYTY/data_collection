492565402853eb7fc4bcfd70987a4fb64f4dfa1a
Stephen <Stephen@CLOUD>
2022-09-06 17:10:26 +0800

    init controller

--- data_collection/Form1.Designer.cs
+++ data_collection/Form1.Designer.cs
@@ -28,13 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1397, 302);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.VirtualMode = true;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "IP Address";
+            this.columnHeader1.Width = 220;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "ID";
+            this.columnHeader2.Width = 115;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Availability";
+            this.columnHeader3.Width = 126;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Virtual";
+            this.columnHeader4.Width = 124;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "SystemName";
+            this.columnHeader5.Width = 134;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Version";
+            this.columnHeader6.Width = 155;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Name";
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Mode";
+            this.columnHeader8.Width = 95;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Guid SystemId";
+            this.columnHeader9.Width = 189;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1632, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 102);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Scan";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 1169);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1397, 280);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2142, 1096);
+            this.ClientSize = new System.Drawing.Size(2856, 1461);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +140,19 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- data_collection/Form1.cs
+++ data_collection/Form1.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ABB.Robotics.Controllers;
 
 namespace data_collection
 {
     public partial class Form1 : Form
     {
+        ABBController robot;
+        List<string> errLog = new List<string>();
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();//InitUserFace
+            robot = new ABBController();
+            //robot.Scan();
+            this.listView1.Items.Clear();
+            robot.Scan();
+            ListViewItem item = null;
+            if (robot.controllers == null) return;
+            for (int i = 0; i < robot.controllers.Count(); i++)
+
+            {
+                item = new ListViewItem(robot.controllers[i].IPAddress.ToString());
+                item.SubItems.Add(robot.controllers[i].Id);
+                item.SubItems.Add(robot.controllers[i].Availability.ToString());
+                item.SubItems.Add(robot.controllers[i].IsVirtual.ToString());
+                item.SubItems.Add(robot.controllers[i].SystemName);
+                item.SubItems.Add(robot.controllers[i].Version.ToString());
+                item.SubItems.Add(robot.controllers[i].ControllerName);
+                item.SubItems.Add(robot.GetController(i).OperatingMode.ToString());
+                item.SubItems.Add(robot.controllers[i].SystemId.ToString());
+                this.listView1.Items.Add(item);
+                item.Tag = robot.controllers[i];
+            }
+            errLog = robot.errLogger(errLog, "[Scan]             " + robot.controllers.Count().ToString() + "  controllers");
+            richTextBox1.Lines = errLog.ToArray();
         }
     }
 }
